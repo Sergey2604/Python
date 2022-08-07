14be6ce225afbeec0f8496c372ea21e006adce65
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-08-07 21:51:20 +0300

    Обновил ключ и гитигнор

--- Семинары/9 урок/ДЗ/main.py
+++ Семинары/9 урок/ДЗ/main.py
@@ -1,9 +1,9 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
-
+import token_bot as token
 import commands as c
 
-updater = Updater("5144126383:AAFE7pLwJnk-EuC0taT6fpQ-6oVzJ7tJk9U")
+updater = token.token_bot
 
 
 updater.dispatcher.add_handler(CommandHandler('start', c.start))
